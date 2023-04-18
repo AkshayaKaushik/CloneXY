e90f4fe2148f6bbf409aee544a80be3523e2ddae
akshitad11 <akshitad@mail.uni-paderborn.de>
2023-04-19 00:03:44 +0200

    Adding all the services' folder structure with empty project to clone xy

---
+++ campaigns-and-promotions/src/main/java/com/clonexy/campaignsandpromotions/CampaignsAndPromotionsApplication.java
@@ -0,0 +1,13 @@
+package com.clonexy.campaignsandpromotions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampaignsAndPromotionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CampaignsAndPromotionsApplication.class, args);
+	}
+
+}
---
+++ campaigns-and-promotions/src/test/java/com/clonexy/campaignsandpromotions/CampaignsAndPromotionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.clonexy.campaignsandpromotions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampaignsAndPromotionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
